9fe9793a4e5b22e15a2950abac1842fd8516c47f
Joshua Simon <joshy.d.simon@gmail.com>
2022-02-24 21:29:24 +1300

    project started

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  interface State {
+      url:string
+  }
+
+  const [ photo,setPhoto ] = useState<State>({
+    url:''
+  })
+
+
+  const getData = async (): Promise<void> => {
+    await axios.get('https://api.nasa.gov/planetary/apod?api_key=9Gu6cwgUJreObBRGtdgrrcFBNRpQzuo4NJjiuPKc')
+    .then(response => {
+      setPhoto({url: response.data.url})
+    })
+  }
+
+  console.log(`this is photo: ${photo}`)
+
+  useEffect(() => {
+    getData()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src = {photo.url}/>
     </div>
   );
 }
