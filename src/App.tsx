7e4aa3887261c82e56960edcb10d547a3021bdbe
Joshua Simon <joshy.d.simon@gmail.com>
2022-02-27 18:13:29 +1300

    axios call done but needs refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
-function App() {
+function App(): JSX.Element {
 
   interface State {
       url:string
   }
 
-  const [ photo,setPhoto ] = useState<State>({
-    url:''
-  })
+  const [ photo,setPhoto ] = useState<State>({url:''})
 
 
   const getData = async (): Promise<void> => {
     await axios.get('https://api.nasa.gov/planetary/apod?api_key=9Gu6cwgUJreObBRGtdgrrcFBNRpQzuo4NJjiuPKc')
-    .then(response => {
-      setPhoto({url: response.data.url})
+    .then((response: AxiosResponse) => {
+      setPhoto(response.data)
     })
   }
 
-  console.log(`this is photo: ${photo}`)
-
+  console.log(photo)
   useEffect(() => {
     getData()
   },[])
 
   return (
     <div>
-      <img src = {photo.url}/>
+      <p>{photo.url}</p>
     </div>
   );
 }
